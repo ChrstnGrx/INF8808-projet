0840c70755272c036d69ad4ed7756577897bdf11
Christine Groux <christine.groux@polymtl.ca>
2024-03-13 13:54:11 -0400

    Add drug info

--- src/constants.py
+++ src/constants.py
@@ -3,9 +3,7 @@ DRUGS = [
     "amphet",
     "amyl",
     "benzos",
-    # "caff",
     "cannabis",
-    # "choc",
     "coke",
     "crack",
     "ecstasy",
@@ -28,3 +26,70 @@ PERSONNALITY = [
     "impulsive",
     "ss"
 ]
+
+DRUG_INFO = {
+    'alcohol': {
+        'group': 'group0',
+        'french': 'alcool'
+    },
+    "amphet": {
+        'group': 'group3',
+        'french': 'amphétamines'
+    },
+    "amyl": {
+        'group': 'group0',
+        'french': 'nitrite d\'amyle'
+    },
+    "benzos": {
+        'group': 'group0',
+        'french': 'benzodiazépine'
+    },
+    "cannabis": {
+        'group': 'group2',
+        'french': 'cannabis'
+    },
+    "coke": {
+        'group': 'group2',
+        'french': 'cocaïne'
+    },
+    "crack": {
+        'group': 'group1',
+        'french': 'crack'
+    },
+    "ecstasy": {
+        'group': 'group2',
+        'french': 'ecstasy'
+    },
+    "heroin": {
+        'group': 'group1',
+        'french': 'héroïne'
+    },
+    "ketamine": {
+        'group': 'group2',
+        'french': 'kétamine'
+    },
+    "legalh": {
+        'group': 'group2',
+        'french': 'euphorisants légaux'
+    },
+    "lsd": {
+        'group': 'group2',
+        'french': 'LSD'
+    },
+    "meth": {
+        'group': 'group3',
+        'french': 'méthadone'
+    },
+    "mushrooms": {
+        'group': 'group2',
+        'french': 'champignons magiques'
+    },
+    "nicotine": {
+        'group': 'group0',
+        'french': 'nicotine'
+    },
+    "vsa": {
+        'group': 'group0',
+        'french': 'substances volatiles'
+    }
+}
